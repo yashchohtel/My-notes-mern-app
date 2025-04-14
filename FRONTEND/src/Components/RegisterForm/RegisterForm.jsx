97df9f7649f64a5fd1e35d4f2cd759d1f0f9870b
yashchohtel <yashchohtel@gmail.com>
2025-04-14 22:57:16 +0530

    welcome page created

--- FRONTEND/src/Components/RegisterForm/RegisterForm.jsx
+++ FRONTEND/src/Components/RegisterForm/RegisterForm.jsx
@@ -1,5 +1,13 @@
 import React, { useState } from 'react'
 import "./registerForm.css"
+import { LuUser } from "react-icons/lu";
+import { MdOutlineDriveFileRenameOutline } from "react-icons/md";
+import { MdOutlineEmail } from "react-icons/md";
+import { MdOutlineLock } from "react-icons/md";
+import { LuEye } from "react-icons/lu";
+import { LuEyeClosed } from "react-icons/lu";
+
+
 
 const RegisterForm = ({ type }) => {
 
@@ -17,11 +25,67 @@ const RegisterForm = ({ type }) => {
     return (
         <>
 
-            {/* Main form containe */}
-            <section className="">
+            {/* Registe and login form */}
+            <div className='register_form'>
+
+                {/* main descrition */}
+                <h2>{formType === "login" ? "login" : "sign in"}</h2>
+
+                {/* secondry descrition */}
+                <p>{formType === 'login' ? 'Login to your account' : 'Create your account'}</p>
+
+                {/* form container */}
+                <form>
+
+                    {/* field for full name for signup */}
+                    {formType === 'signup' && (
+                        <div className="input_group">
+                            <span> <MdOutlineDriveFileRenameOutline /> </span>
+                            <input type="text" placeholder="Full Name" required />
+                        </div>
+                    )}
+
+                    {/* field for unique username name for signup */}
+                    {formType === 'signup' && (
+                        <div className="input_group">
+                            <span> <MdOutlineDriveFileRenameOutline /> </span>
+                            <input type="text" placeholder="Username" required />
+                        </div>
+                    )}
+
+                    {/* field for identifier for login */}
+                    {formType === 'login' && (
+                        <div className="input_group">
+                            <span> <LuUser /> </span>
+                            <input type="text" placeholder="Username Or Email" required />
+                        </div>
+                    )}
 
 
-            </section>
+                    {/* field for full name for signup */}
+                    {formType === 'signup' && (
+                        <div className="input_group">
+                            <span> <MdOutlineEmail /> </span>
+                            <input type="email" placeholder="Email" required />
+                        </div>
+                    )}
+
+
+                    <div className="input_group">
+                        <span> <MdOutlineLock /> </span>
+                        <input type="password" placeholder="Password" required />
+                        <span> <LuEyeClosed /> </span>
+                    </div>
+
+                    <button type="submit" className='button_primary'>Sign Up</button>
+
+                    <p className=''>Already have an account ?
+                        <span onClick={() => toggleForm()}>Login here</span>
+                    </p>
+
+                </form>
+
+            </div>
 
         </>
     )
